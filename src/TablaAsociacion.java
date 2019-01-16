ed47b150f8397447b57123f3458fea12660d438f
Claudia Gázquez Alessio <claudia365x@gmail.com>
2019-01-16 12:24:18 +0100

    Añadidos atributos y métodos

--- src/TablaAsociacion.java
+++ src/TablaAsociacion.java
@@ -1,2 +1,9 @@
 public class TablaAsociacion {
+    //Atributos
+    private Character tabla = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+
+    //Métodos
+    public Character calcularLetra() {
+        
+    }
 }
