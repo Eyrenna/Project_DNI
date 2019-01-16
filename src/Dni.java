d93d8a910839841c924a6d0437751d981f08b2cd
Claudia Gázquez Alessio <claudia365x@gmail.com>
2019-01-16 12:06:44 +0100

    Añadidos atributos y métodos

--- src/Dni.java
+++ src/Dni.java
@@ -1,2 +1,20 @@
 public class Dni {
+    private String dni = null;
+    private TablaAsociacion tabla = new TablaAsociacion();
+
+    public String getDni() {
+        return this.dni;
+    }
+    public String getParteNumerica() {
+
+    }
+    public String getLetraDni() {
+
+    }
+    public Boolean verificarLetra() {
+
+    }
+    private Character calcularLetra() {
+
+    }
 }
